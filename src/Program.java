3d8e5d497f40976ef67fc054e8121b935915bebe
F-I-D-O <david.fido.fiedler@gmail.com>
2015-10-07 18:54:20 +0200

    init commit

---
+++ src/Program.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david_000
+ */
+public class Program {
+    
+    public static void main (String[] arguments){
+        System.out.println("tst");
+    }
+}
